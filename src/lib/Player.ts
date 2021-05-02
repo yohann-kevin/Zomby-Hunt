f2b21efad04fc955534185c0e5f84de4768f7fef
Kirua <yo44prg@icloud.com>
2021-05-02 23:03:29 +0200

    feat: add animation

--- src/lib/Player.ts
+++ src/lib/Player.ts
@@ -4,11 +4,13 @@ export default class Player {
     name: string;
     game: Game;
     playerOne: any;
+    isJumping: boolean;
 
     constructor (game: Game) {
         this.name = "kirua";
         this.game = game;
         this.playerOne = null;
+        this.isJumping = false;
     }
 
     initPlayer () {
@@ -26,18 +28,42 @@ export default class Player {
             }),
             frameRate: 5,
             repeat: -1
-        })
+        });
+        this.game.anims.create({
+            key: "playerIdle",
+            frames: [{key: "adventurer", frame: "adventurer_stand"}, {key: "adventurer", frame: "adventurer_idle"}],
+            frameRate: 2,
+            repeat: -1
+        });
     }
 
-    manageMove() {
+    manageMove() { 
         if (this.game.cursors.left.isDown) {
             this.playerOne.setVelocityX(-200);
-            this.playerOne.play("playerWalk");
+            this.playerOne.setFlip(true, false);
         } else if (this.game.cursors.right.isDown) {
             this.playerOne.setVelocityX(+200);
-            this.playerOne.play("playerWalk");
+            this.playerOne.setFlip(false, false);
         } else {
             this.playerOne.setVelocityX(0);
+        }
+        
+        if(this.game.cursors.up.isDown && this.playerOne.body.onFloor()) {
+            this.playerOne.setVelocityY(-350);
+        }
+
+        this.playerOne.body.onFloor() ? this.isJumping = false : this.isJumping = true;
+
+        if (this.isJumping) {
+            this.playerOne.setTexture("adventurer", "adventurer_jump");
+        } else {
+            if (this.game.cursors.left.isDown) {
+                this.playerOne.play("playerWalk", true);
+            } else if (this.game.cursors.right.isDown) {
+                this.playerOne.play("playerWalk", true);
+            } else {
+                this.playerOne.play("playerIdle", true);
+            }
         }
     }
 }
